21109cc17d9a41410941ff0b575e334e82e509c4
RichardHitt <rbh00@netcom.com>
2015-02-18 09:27:55 -0800

    For Alpha testing, force extra message spacing to 3

--- src/common/cfgfiles.c
+++ src/common/cfgfiles.c
@@ -835,6 +835,7 @@ load_default_config(void)
 	prefs.hex_notify_timeout = 15;
 	prefs.hex_text_max_indent = 256;
 	prefs.hex_text_max_lines = 500;
+	prefs.hex_text_spacing_msg = 3;
 	prefs.hex_url_grabber_limit = 100; 		/* 0 means unlimited */
 
 	/* STRINGS */
